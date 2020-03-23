aaf7aa72d6c0fa9b2f4a4afa5db2400ccb1f815f
Willy <wzhengwei6@gmail.com>
2020-03-23 20:22:16 +0800

    main.c

--- hw1/main.c
+++ hw1/main.c
@@ -1,26 +1,101 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
+#include <getopt.h>
+#include <regex.h> 
 #include "dir_proc.h"
 #include "tcp_proc.h"
+#include "tcp6_proc.h"
 
 #define PROC_DIR "/proc/"
 #define TCP4_PATH "/proc/net/tcp"
+#define TCP6_PATH "/proc/net/tcp6"
+#define UDP4_PATH "/proc/net/udp"
+#define UDP6_PATH "/proc/net/udp6"
+
+int mode = 0;
+char filter_str[100];
+regex_t regex;
 
 int main(int argc, char *argv[]) {
+
+    memset(filter_str, '\0', 100);
+    int c;
+    int option_index = 0;
+    int select = 0;
+    const char *optstring = "tu";
+    struct option opts[] = {
+        {"tcp", 0, NULL, 't'},
+        {"udp", 0, NULL, 'u'}
+    };
+    while((c = getopt_long(argc, argv, optstring, opts, &option_index)) != -1) {
+        char *tmp_optarg = optarg;
+        switch(c) {
+            case 't':
+                select = select + 1;
+                break;
+            case 'u':
+                select = select + 2;
+                break;
+        }
+    }
+    if (argc > select) {
+        strcpy(filter_str, argv[argc-1]);
+        regcomp(&regex, filter_str, 0);
+        
+    }
+
+
     int net_table_length;
     int dir_table_length;
     int i;
     char **dir_table;
-    printf("%s\n","Proto Local Address           Foreign Address         PID/Program name and arguments");
-    // build tcp_table
-    net_tcp4 *net_table = create_tcp4_table(&net_table_length, TCP4_PATH);
-    if (net_table == NULL) {
-        return -1;
+    net_tcp4 *net_table;
+    net_tcp6 *net6_table;
+
+    if ((select & 1) >= 1){
+        mode = 0;
+        printf("%s\n", "List of TCP connections:");
+        printf("%s\n", "Proto Local Address                                "
+                    "Foreign Address                              "        
+                    "PID/Program name and arguments");
+        // build tcp4_table
+        
+        net_table = create_tcp4_table(&net_table_length, TCP4_PATH);
+        if (net_table == NULL) {
+            return -1;
+        }
+        tcp4_address_form(net_table, net_table_length);
+        netstat_tcp4(net_table, PROC_DIR, net_table_length);
+        
+        // build tcp6_table
+        net6_table = create_tcp6_table(&net_table_length, TCP6_PATH);
+        if (net6_table == NULL) {
+            return -1;
+        }
+        tcp6_address_form(net6_table, net_table_length);
+        netstat_tcp6(net6_table, PROC_DIR, net_table_length);
+        printf("\n");
     }
-    print_tcp4_table(net_table, net_table_length);
-    // read /proc/[pid]/fd/
-    netstat_tcp4(net_table, PROC_DIR, net_table_length);
+    if ((select & 2) >= 1) {
+        mode = 1;
+        printf("%s\n", "List of UDP connections:");
+        printf("%s\n", "Proto Local Address                                "
+                    "Foreign Address                              "        
+                    "PID/Program name and arguments");
+        net_table = create_tcp4_table(&net_table_length, UDP4_PATH);
+        if (net_table == NULL) {
+            return -1;
+        }
+        tcp4_address_form(net_table, net_table_length);
+        netstat_tcp4(net_table, PROC_DIR, net_table_length);
+        
+        net6_table = create_tcp6_table(&net_table_length, UDP6_PATH);
+        if (net6_table == NULL) {
+            return -1;
+        }
+        tcp6_address_form(net6_table, net_table_length);
+        netstat_tcp6(net6_table, PROC_DIR, net_table_length);
+    }
     return 0;
 }
-char *a = "Local Address           ";
